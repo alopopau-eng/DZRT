4e8b36d51efc06a9954f0cc6b7a480b7c4bea416
alopopau-eng <alopopau@gmail.com>
2025-12-16 17:13:12 +0300

    ssss

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -3,12 +3,16 @@ import { ProductGrid } from "@/components/home/ProductGrid";
 import { InfoSection } from "@/components/home/InfoSection";
 import { Footer } from "@/components/layout/Footer";
 import { Hero } from "@/components/home/Hero";
+import { useState } from "react";
+import { FullPageLoader } from "@/components/Loader";
 
 export default function Home() {
+  const [loading,setLoading]=useState(true)
   return (
     <div className="min-h-screen bg-black text-white overflow-x-hidden">
       <Navbar />
       <main>
+        {loading&&<FullPageLoader/>}
         <Hero />
         <InfoSection />
         <ProductGrid />
